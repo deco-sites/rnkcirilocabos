6b8fa032c1e0195ee26ad286fbee19845d290cfc
bryamatsgit <bryamatss@gmail.com>
2024-01-31 11:19:41 -0300

    Section - SoloBanner - Adicionando marginY

--- sections/SoloBanner.tsx
+++ sections/SoloBanner.tsx
@@ -1,4 +1,8 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+import { clx } from "../sdk/clx.ts";
+import { margin } from "../constants.tsx";
 import Image from "apps/website/components/Image.tsx";
+import Container from "./Layout/Container.tsx";
 
 export interface Props {
   /**
@@ -8,19 +12,53 @@ export interface Props {
   /**
    * @description Source (URL) da Imagem
    */
-  src: string;
+  src: ImageWidget;
+  /**
+   * @default "default"
+   * @description margin no eixo Y (cima e baixo)
+   */
+  deskMarginY?: "none" | "sm" | "default" | "lg" | "xl" | "xxl";
+  /**
+   * @default "default"
+   * @description margin no eixo Y (cima e baixo)
+   */
+  mobiMarginY?: "none" | "sm" | "default" | "lg" | "xl" | "xxl";
 }
 
-export default function SoloBanner({ alt, src }: Props) {
+function GetSoloBanner(
+  {
+    alt,
+    src,
+    deskMarginY,
+    mobiMarginY,
+  }: Props,
+) {
   return (
-    <div class="container px-2">
-      <Image
-        src={src}
-        alt={alt}
-        width={1376}
-        height={65}
-        class="object-cover h-auto mx-auto"
-      />
-    </div>
+    <Image
+      src={src}
+      alt={alt}
+      width={1376}
+      height={65}
+      class={clx(
+        "object-cover h-auto mx-auto",
+        margin.y.desk[deskMarginY ?? "none"],
+        margin.y.mobi[mobiMarginY ?? "none"]
+      )}
+    />
   );
 }
+
+function SoloBanner(props: Props) {
+  return (
+    <>
+      <Container
+        children={{
+          Component: GetSoloBanner,
+          props: { ...props },
+        }}
+      />
+    </>
+  );
+}
+
+export default SoloBanner;

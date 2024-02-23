02d32044de87a9845c830bd17900fc005641c599
bryamatsgit <bryamatss@gmail.com>
2024-02-23 11:32:34 -0300

    SDK - Criando Curry e Debounce + Ajustando useVariantPossibilities

--- sdk/curry.ts
+++ sdk/curry.ts
@@ -3,15 +3,15 @@ export const curry = <F extends Function>(fn: F): Function => {
   try {
     // deno-lint-ignore no-explicit-any
     return function curried<T>(this: any, ...args: T[]) {
-      if(args.length >= fn.length) {
+      if (args.length >= fn.length) {
         return fn.apply(this, args);
       } else {
         return (...args2: T[]) => {
           curried.apply(this, args.concat(args2));
-        }
+        };
       }
-    }
+    };
   } catch (e) {
     throw new Error(e);
   }
-}
+};
--- sdk/debounce.ts
+++ sdk/debounce.ts
@@ -2,16 +2,16 @@
 export const debounce = <F extends Function>(fn: F, time = 300): Function => {
   try {
     let timer = setTimeout(() => {});
-    
+
     const debounced = <T>(...args: T[]): void => {
       clearTimeout(timer);
       timer = setTimeout(() => {
         fn.apply(this, args);
       }, time);
-    }
+    };
 
     return debounced;
   } catch (e) {
     throw new Error(e);
   }
-}
+};
